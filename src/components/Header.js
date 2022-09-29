b0eb918b4a4de12bdcbda1e973df27f2d28ada00
MideGiwa <olamdegiwa21@gmail.com>
2022-09-29 02:59:44 +0100

    Re: Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ export default function Header() {
     return (
         <header className="header">
             <h1>Speller</h1>
-            <div className="search">
+            {/* <div className="search">
                 <input type="text" placeholder='Try to type it out..'></input>
-            </div>
+            </div> */}
             <a href="#">Word of the day!</a>
         </header>
     )
